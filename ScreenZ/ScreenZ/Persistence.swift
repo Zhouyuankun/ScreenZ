29d4c6a527f6f8c625d9fcd1759e7740e9924646
Zhouyuankun <zykun7525599>
2024-03-24 00:12:07 +0800

    File operations moved to here

--- ScreenZ/ScreenZ/Persistence.swift
+++ ScreenZ/ScreenZ/Persistence.swift
@@ -10,24 +10,6 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-//    static var preview: PersistenceController = {
-//        let result = PersistenceController(inMemory: true)
-//        let viewContext = result.container.viewContext
-//        for _ in 0..<10 {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//        }
-//        do {
-//            try viewContext.save()
-//        } catch {
-//            // Replace this implementation with code to handle the error appropriately.
-//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//            let nsError = error as NSError
-//            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//        }
-//        return result
-//    }()
-
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
@@ -53,4 +35,92 @@ struct PersistenceController {
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
+    
+    func getCurrentVideoURL() -> URL? {
+        let context = self.container.viewContext
+        let fetchRequest = Preference.fetchRequest()
+        let result = try! context.fetch(fetchRequest).first!
+        let url = result.currentVideoURL
+        //Check if URL is valid
+        if url != nil && FileManager.default.fileExists(atPath: url!.path()) {
+            return url
+        } else {
+            return nil
+        }
+        
+    }
+    
+    func storeURL(url: URL) {
+        let context = self.container.viewContext
+        let fetchRequest = Preference.fetchRequest()
+        let result = try! context.fetch(fetchRequest).first!
+        result.currentVideoURL = url
+        try! self.container.viewContext.save()
+    }
+    
+    func deleteAllEntryVideo() {
+        let context = self.container.viewContext
+        let fetchRequest = Video.fetchRequest()
+        let result = try! context.fetch(fetchRequest)
+        for v in result {
+            context.delete(v)
+        }
+    }
+    
+    func deleteAllEntryPreference() {
+        let context = self.container.viewContext
+        let fetchRequest = Preference.fetchRequest()
+        let result = try! context.fetch(fetchRequest)
+        for v in result {
+            context.delete(v)
+        }
+    }
+    
+    func fileExists(_ name: String) -> Bool {
+        let fetchRequest = Video.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "name == %@", name)
+        let result = try! self.container.viewContext.fetch(fetchRequest)
+        return result.count == 1
+    }
+    
+    func renameVideo(src: String, dst: String) {
+        let fetchRequest = Video.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "name == %@", src)
+        let result = try! self.container.viewContext.fetch(fetchRequest).first!
+        result.name = dst
+        try! container.viewContext.save()
+    }
+    
+    func cleanFolder() {
+        let fetchRequest = Video.fetchRequest()
+        let viewContext = PersistenceController.shared.container.viewContext
+        let result = try! viewContext.fetch(fetchRequest)
+        let names = result.map {$0.name!}
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURLs = try! FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil,options: .skipsHiddenFiles)
+        let trashs = fileURLs.filter {!names.contains($0.getFileName()!)}
+        for trash in trashs {
+            try! FileManager.default.removeItem(at: trash)
+        }
+    }
+    
+    func deleteAllThemes() {
+        let fetchRequest = Video.fetchRequest()
+        let viewContext = PersistenceController.shared.container.viewContext
+        let result = try! viewContext.fetch(fetchRequest)
+        for res in result {
+            viewContext.delete(res)
+        }
+        
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil,options: .skipsHiddenFiles)
+            for fileURL in fileURLs {
+                try FileManager.default.removeItem(at: fileURL)
+            }
+        } catch  { print(error) }
+    }
+    
+    
 }
